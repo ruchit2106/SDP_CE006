bc14ab6c3a4749deec0eb388c6bcc522b2a87636
ruchit2106 <87220033+ruchit2106@users.noreply.github.com>
2022-09-19 21:16:18 +0530

    Update main.dart

--- LAB9/lab9/lib/main.dart
+++ LAB9/lab9/lib/main.dart
@@ -1,8 +1,12 @@
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:lab9/quote.dart';
 import 'package:lab9/quote_card.dart';
+import 'package:lab9/home.dart';
+import 'package:lab9/loading.dart';
+import 'package:lab9/choose_location.dart';
 
-void main() => runApp(MaterialApp(
+//Code Test 1
+/*void main() => runApp(MaterialApp(
   home: EchoList(),
 ));
 class EchoList extends StatefulWidget {
@@ -11,57 +15,97 @@ class EchoList extends StatefulWidget {
   State<EchoList> createState() => _EchoListState();
 }
 class _EchoListState extends State<EchoList> {
-
   List<Quote> quotes = [
     Quote(text: 'The truth is realy pure and never simple',author:
     'Ruchit'),
-    Quote(author: 'Ruchit1', text: 'I see humans but no humanity'),
+    Quote(author: 'Ruchit2', text: 'I see humans but no humanity'),
     Quote(text: 'The time is always right to do what is right',author:
-    'Ruchit2'),
+    'Ruchit3'),
   ];
 
-  /*Widget quoteTemplate(quote){
-    return QuoteCard(quote:quote,);
-  }*/
+  Widget quoteTemplate(quote){
+    return QuoteCard(quote:quote);
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent[100],
       appBar: AppBar(
-        title: Text('Quotes'),
+        title: Text("Today's Quotes"),
         centerTitle: true,
-        backgroundColor: Colors.orangeAccent,
+        backgroundColor: Colors.green,
       ),
       body: Column(
-        children: quotes.map((quote) => QuoteCard(
-          quote: quote,
-          delete: () {
-            setState(() {
-              quotes.remove(quote);
-            });
-          },
-        )).toList(),
-
-// mainAxisAlignment: MainAxisAlignment.center,
-// crossAxisAlignment: CrossAxisAlignment.center,
-        // children: quotes.map((quote) => quoteTemplate(quote)).toList(),
-        //children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
       ),
     );
   }
+
+
+  */
+
+/* // Code Test 2
+void main() => runApp(MaterialApp(
+home: EchoList(),
+));
+class EchoList extends StatefulWidget {
+const EchoList({Key? key}) : super(key: key);
+@override
+State<EchoList> createState() => _EchoListState();
+}
+class _EchoListState extends State<EchoList> {
+
+List<Quote> quotes = [
+Quote(text: 'The truth is realy pure and never simple',author:
+'Ruchit'),
+Quote(author: 'Ruchit1', text: 'I see humans but no humanity'),
+Quote(text: 'The time is always right to do what is right',author:
+'Ruchit2'),
+];
+
+/*Widget quoteTemplate(quote){
+    return QuoteCard(quote:quote,);
+  }*/
+
+@override
+Widget build(BuildContext context) {
+
+return Scaffold(
+backgroundColor: Colors.lightBlueAccent[100],
+appBar: AppBar(
+title: Text('Quotes'),
+centerTitle: true,
+backgroundColor: Colors.orangeAccent,
+),
+body: Column(
+children: quotes.map((quote) => QuoteCard(
+quote: quote,
+delete: () {
+setState(() {
+quotes.remove(quote);
+});
+},
+)).toList(),
+
+// mainAxisAlignment: MainAxisAlignment.center,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+//children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
+),
+);
+}
 }*/
-import 'dart:ui';
-import 'package:flutter/material.dart';
-import 'choose_location.dart';
-import 'home.dart';
-import 'loading.dart';
 
-import 'package:flutter/material.dart';
+// lab 9 tutorial 2
+void main() => runApp(MaterialApp(
+// home: Home(),
+// instead of making home: property to make any page to initialize at
 
-void main() => runApp(MaterialApp(initialRoute: '/home', routes: {
-  '/': (context) => Loading(),
-  '/home': (context) => Home(),
-  '/location': (context) => ChooseLocation(),
-}));
+// we can use following code ....
+        initialRoute: '/home',
+        routes: {
+          '/': (context) => Loading(),
+          '/home': (context) => Home(),
+          '/location': (context) => ChooseLocation(),
+        }));
