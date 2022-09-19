a38cb689fd49a44f8923fc6c0ff056ab031cd0b8
ruchit2106 <87220033+ruchit2106@users.noreply.github.com>
2022-09-19 20:35:50 +0530

    Add files via upload

---
+++ LAB6/lab6/lib/main.dart
@@ -0,0 +1,39 @@
+///LAB6
+
+
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomeScreen(),
+    ));
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HELLO FLUTTER...MY FIRST APP'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'HELLO DDU',
+          style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'Aboreto'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
